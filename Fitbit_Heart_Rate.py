fcec68cb458c54a01b37ab84170a5b50532afe7d
rslinford <scott@linfords.com>
2017-01-25 09:21:23 -0800

    OAuth config items

--- Fitbit_Heart_Rate.py
+++ Fitbit_Heart_Rate.py
@@ -2,6 +2,10 @@ import rsl.json_config as json_config
 
 default_config = {
    'config_file_name': r'fitbit-heart-rate.json',
+   "OAuth_Client_ID": "MyID",
+   "Client_Secret": "MySecret",
+   "OAuth_Authorization_URI": "https://www.fitbit.com/oauth2/authorize",
+   "OAuth_Token_Request_URI": "https://api.fitbit.com/oauth2/token"
 }
 
 def read_data(config):
